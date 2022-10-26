76c21a3fa054582d6b26de8ec0543d9819500f1e
Evanraboutot <95092081+Evanraboutot@users.noreply.github.com>
2022-10-27 00:38:11 +0200

    Src init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
-  <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu (dans <code class="font-mono">/src/App.vue</code>)
-    </h4>
-    <ul>
-      <li>
-        <router-link class="text-red-600 underline" to="/">
-          lien vers
-          <code class="font-mono">/src/pages/index.vue</code>
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+  <div class="relative overflow-hidden bg-black"></div>
 
+  <div>
+          <div class="relative px-4 pt-6 sm:px-6 lg:px-8 bg-stone-900">
+            <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+              <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+                <div class="flex w-full items-center justify-between md:w-auto">
+                  <a href="#">
+                    <img alt="Your Company" class="h-8 w-auto sm:h-10" src="../public/Group 97.png">
+                  </a>
+                  <div class="-mr-2 flex items-center md:hidden">
+                    <button type="button" class="inline-flex items-center justify-center rounded-md bg-bl p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                      <span class="sr-only">Open main menu</span>
+                      <!-- Heroicon name: outline/bars-3 -->
+                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div class="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
+                <a href="/Accueil" class="font-medium text-white hover:bg-stone-800">Acceuil</a>
+  
+                <a href="/SvgFormView" class="font-medium text-white hover:bg-stone-800">Personnalisation</a>
+
+                <a href="/" class="font-medium text-white hover:bg-stone-800">Connexion</a>
+              </div>
+            </nav>
+          </div>
+  </div>
   <!-- Affiche les pages -->
-  <Suspense>
-    <router-view class="m-2 border-2 p-2" />
-  </Suspense>
+  <router-view />
 </template>
 
 <script setup lang="ts">
-import { Bars3Icon } from "@heroicons/vue/20/solid";
+import { Bars3Icon } from '@heroicons/vue/20/solid'
 </script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@formkit/themes/genesis'
+
+createApp(App).mount('#app')
